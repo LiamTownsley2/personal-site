914d4608c87c036c3cd025ddd3bc6f7b607a8663
Liam Townsley <contact@liamtownsley.me>
2025-05-11 02:50:52 +0100

    refactor: hide discord webhook URL from public codebase

--- src/lib/discord.ts
+++ src/lib/discord.ts
@@ -1,19 +1,21 @@
 "use server";
 export async function postWebhook(firstName: string, lastName: string, email: string, message: string) {
-    const response = await fetch('https://discord.com/api/webhooks/1370471620959010932/xx9b-m7zOOIz5YeJgS8F01HB3Tx2B2v36wjAz7hZSI4GXdtQ0Oo1w195ggmlIueYiN0p', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            content: [
-                `<@867176043987075072>`,
-                `**First Name**: ${firstName}`,
-                `**Last Name**: ${lastName}`,
-                `**Email**: ${email}`,
-                `**Message**:\n\`\`\`${message.replace("`", "'")}\`\`\``,
-            ].join('\n')
-        }),
-    })
-    return response.ok;
+    if (process.env.DISCORD_WEBHOOK_URL) {
+        const response = await fetch(process.env.DISCORD_WEBHOOK_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                content: [
+                    `<@867176043987075072>`,
+                    `**First Name**: ${firstName}`,
+                    `**Last Name**: ${lastName}`,
+                    `**Email**: ${email}`,
+                    `**Message**:\n\`\`\`${message.replace("`", "'")}\`\`\``,
+                ].join('\n')
+            }),
+        })
+        return response.ok;
+    }
 }
