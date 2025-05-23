a775bf57fd5c1b8c00037a8c55c0edb2d9562cba
Liam Townsley <contact@liamtownsley.me>
2025-05-23 17:50:45 +0100

    feat: add drafts to blog posts

--- src/components/BlogPostPreviewGrid.tsx
+++ src/components/BlogPostPreviewGrid.tsx
@@ -4,15 +4,19 @@ import { PostType } from "@/models/post"
 import { fetcher } from "@/lib/utils"
 import useSWR from 'swr'
 
-export default function BlogPostPreviewGrid() {
+export default function BlogPostPreviewGrid({ showDrafts = false }: { showDrafts?: boolean }) {
     const { data, error } = useSWR('/api/posts', { fetcher })
 
     if (error) return <div className="text-center">Failed to load</div>
     if (!data) return <div className="text-center">Loading...</div>
 
+    const filteredPosts = showDrafts
+    ? data
+    : data.filter((post: PostType) => !post.published);
+
     return (
         <div className="mx-auto grid max-w-5xl grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-            {data.map((post: PostType) => (
+            {filteredPosts.map((post: PostType) => (
                 <BlogPostPreview key={post._id.toString()} post={post} />
             ))}
         </div>
